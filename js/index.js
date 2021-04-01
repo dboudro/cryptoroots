1ccc70769ba6de8ed5acb8a116a50b80d3a72562
dboudro <dylanrboudro@gmail.com>
2021-03-31 23:05:07 -0700

    rebrand

--- js/index.js
+++ js/index.js
@@ -10,158 +10,13 @@
             search: '',
             bookmarks: [
                 {
-                    text: 'Material Design Dev Tutorials',
-                    hyperlink: 'https://material.io/collections/developer-tutorials/',
+                    text: 'Test Link',
+                    hyperlink: 'https:zksync.io',
                     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
                     taglist: ['testing', 'tag2'],
                     newtag: '',
                     favorited: false,
                     lastlocation: -1
-                },
-                {
-                    text: 'Awesome Vue.js Resources',
-                    hyperlink: 'https://github.com/vuejs/awesome-vue#youtube-channels',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'HackerNews',
-                    hyperlink: 'https://news.ycombinator.com/',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'Zenhabits',
-                    hyperlink: 'https://zenhabits.net',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'How to Get Rich by Naval Ravikant',
-                    hyperlink: 'https://twitter.com/naval/status/1002103360646823936',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'How to Be Successful by Sam Altman',
-                    hyperlink: 'http://blog.samaltman.com/how-to-be-successful',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'Design in Tech Report 2019',
-                    hyperlink: 'https://designintech.report/2019/03/09/design-in-tech-report-2019/',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: '1000 True Fans',
-                    hyperlink: 'https://kk.org/thetechnium/1000-true-fans/',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'Typewolf Adobe Fonts',
-                    hyperlink: 'https://www.typewolf.com/adobe-fonts',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'Morning Rituals by Tim Ferriss ',
-                    hyperlink: 'https://fhww.files.wordpress.com/2018/09/tim-ferriss-5-morning-rituals.pdf',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'CSSTricks ',
-                    hyperlink: 'https://css-tricks.com',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'Google 2004 Founder Letter',
-                    hyperlink: 'https://abc.xyz/investor founders-letters/2004-ipo-letter/',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'Pentagram',
-                    hyperlink: 'http://www.pentagram.com/work/#/all/all/newest/',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'Sagmeister & Walsh',
-                    hyperlink: 'https://sagmeisterwalsh.com/work/',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'Harvard Meta Lab',
-                    hyperlink: 'https://metalabharvard.github.io/',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'MIT Media Lab',
-                    hyperlink: 'https://www.media.mit.edu/research/?filter=projects',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel lectus ut turpis fermentum vestibulum. ',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                },
-                {
-                    text: 'JSON Placeholder API',
-                    hyperlink: 'https://jsonplaceholder.typicode.com/',
-                    description: 'A fake online REST API tool for prototyping / testing simple http requests',
-                    taglist: ['testing', 'tag2'],
-                    newtag: '',
-                    favorited: false,
-                    lastlocation: -1
-                                  
                 }
 
         ]},
@@ -183,7 +38,7 @@
                 this.toggleShow()
 
                 // post to firebase
-                this.$http.post('https://roots-network-69742.firebaseio.com/chromes.json', newbookmark).then(function (data) {
+                this.$http.post('https://roots-network-69742.firebaseio.com/crypto.json', newbookmark).then(function (data) {
                   console.log(data)
                 })
             },
@@ -202,14 +57,14 @@
                     app.bookmarks.splice(linkLocation, 1)
                     app.bookmarks.unshift(link);
                     link.favorited = !link.favorited;
-                    this.$http.put('https://roots-network-69742.firebaseio.com/chromes.json', this.bookmarks).then(function (data) {
+                    this.$http.put('https://roots-network-69742.firebaseio.com/crypto.json', this.bookmarks).then(function (data) {
                         console.log('PUT FAV')
                     })
 
                 }
             },
             put: function (link) {
-                this.$http.put('https://roots-network-69742.firebaseio.com/chromes.json', this.bookmarks).then(function (data) {
+                this.$http.put('https://roots-network-69742.firebaseio.com/crypto.json', this.bookmarks).then(function (data) {
                     alert('Database Updated Succesfully');
                 })
             },
@@ -219,7 +74,7 @@
                 app.bookmarks.splice(linkLocation, 1)
                 app.bookmarks.splice(link.lastlocation, 0, link)
                 link.favorited = !link.favorited;
-                this.$http.put('https://roots-network-69742.firebaseio.com/chromes.json', this.bookmarks).then(function (data) {
+                this.$http.put('https://roots-network-69742.firebaseio.com/crypto.json', this.bookmarks).then(function (data) {
                     console.log('PUT UNFAV')
                 })
             },
@@ -227,8 +82,8 @@
                 let linkLocation = app.bookmarks.indexOf(link)
                 if (linkLocation !== -1 && confirm("Are you sure you want to delete this bookmark?")) {
                     app.bookmarks.splice(linkLocation, 1)
-                    this.$http.delete('https://roots-network-69742.firebaseio.com/chromes/' + link.id + '/.json', link.id).then(function (data) {
-                        console.log('https://roots-network-69742.firebaseio.com/chromes/' + link.id + '/.json');
+                    this.$http.delete('https://roots-network-69742.firebaseio.com/crypto/' + link.id + '/.json', link.id).then(function (data) {
+                        console.log('https://roots-network-69742.firebaseio.com/crypto/' + link.id + '/.json');
                     })
                 }
             },
@@ -283,14 +138,14 @@
                   link.taglist.splice(i, 1);
                 }
               }
-              this.$http.put('https://roots-network-69742.firebaseio.com/chromes.json', this.bookmarks).then(function (data) {
+              this.$http.put('https://roots-network-69742.firebaseio.com/crypto.json', this.bookmarks).then(function (data) {
                     console.log('PUT removetag')
                 })
             },
             addTag: function (link, tag) {
              link.taglist.push(link.newtag.toLowerCase());
              link.newtag = "";
-             this.$http.put('https://roots-network-69742.firebaseio.com/chromes.json', this.bookmarks).then(function (data) {
+             this.$http.put('https://roots-network-69742.firebaseio.com/crypto.json', this.bookmarks).then(function (data) {
                     console.log('PUT addTag')
                 })
             },
@@ -300,7 +155,7 @@
         },
 
         created() {
-            this.$http.get('https://roots-network-69742.firebaseio.com/chromes.json').then(function (data) {
+            this.$http.get('https://roots-network-69742.firebaseio.com/crypto.json').then(function (data) {
                 // this.bookmarks=data.body;
                 return data.json()
             }).then(function (data) {
